61df84b68739ca20e2711bcb114bd93581947b70
phenyo-code <phenyo.koikoi@icloud.com>
2025-03-31 00:33:47 +0200

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,14 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+      <meta name="google-site-verification" content="JdE3z7wZOa95HAumRV1e8QtaDb6Y2qApfN5Uu4meVfw" />
+      <meta name="robots" content="index, follow" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="theme-color" content="#6393FF" />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" href="/logo.png" />
+      </head>
       <body className={poppins.className}>
         <Header />
         <main style={{ paddingTop: '4rem' }}>{children}</main>
